6d91ece4fa1d2e1b8457ee29855838024c449521
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-06-09 09:12:40 +0300

    feat(app): add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <router-view/>
+  <div class="app flex">
+    <navigation />
+    <div class="content flex flex-column">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
+import Navigation from '@/components/Navigation.vue';
+
 export default {
   name: 'App',
+  components: {
+    Navigation,
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.app {
+  background-color: #141625;
+  min-height: 100vh;
+  flex-direction: column;
+
+  @media (min-width: 900px) {
+    flex-direction: row;
+  }
+
+  .content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="header flex">
+    <div class="brand flex">
+      <img
+        class="logo"
+        src="@/assets/images/file-invoice-dollar-solid.png"
+        alt=""
+      />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  z-index: 99;
+  flex-direction: row;
+  background-color: #1e2139;
+
+  @media (min-width: 900px) {
+    min-height: 100%;
+    min-width: 90px;
+    flex-direction: column;
+    border-radius: 0 20px 20px 0;
+  }
+
+  .brand {
+    border-radius: 0 20px 20px 0;
+    background-color: #7c5dfa;
+    justify-content: center;
+    padding: 24px;
+
+    @media (min-width: 900px) {
+      width: 100%;
+    }
+
+    .logo {
+      width: auto;
+      height: 30px;
+    }
+  }
+}
+</style>
