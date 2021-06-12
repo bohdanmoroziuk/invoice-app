8699d14404d1864c6aa872e652726dfb827c400f
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-06-12 14:58:32 +0300

    fix(invoice-modal): hide scrollbar

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -210,6 +210,8 @@ export default {
   width: 100%;
   height: 100vh;
   overflow: scroll;
+  -ms-overflow-style: none; /* IE 11 */
+  scrollbar-width: none; /* Firefox 64 */
 
   &::-webkit-scrollbar {
     display: none;
