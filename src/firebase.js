0bba4792a21bac99bbc4e4d30b6fd22a90d2edc6
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-06-13 09:00:24 +0300

    chore(firebase): initialize app, create firestore

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+};
+
+const app = firebase.initializeApp(config);
+
+export const firestore = app.firestore();
+
+export default app;
